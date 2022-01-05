f3ac365ca99e918e56bd9e926e2211b7b0b4a583
smilingfroggy <smilingfroggy@hotmail.com>
2022-01-06 01:30:57 +0800

    feat: Post order, POST order/cancel

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,15 +8,60 @@ const dottie = require('dottie')
 const orderController = {
   getOrders: (req, res) => {
     Order.findAll({
-      // raw: true,  // orders, items OK 但OrderItem還是[Object]
-      // nest: true,  // items還是被攤平到orders，變成每個order一個items & product
       include: 'items',
-      // include: { model: Product, as: 'items' }  // 結果同上
     }).then(orders => {
       console.log('orders:', JSON.parse(JSON.stringify(orders)))
       orders = JSON.parse(JSON.stringify(orders))
       return res.render('orders', { orders })
     })
+  },
+  postOrder: (req, res) => {
+    return Cart.findByPk(req.body.cartId, {
+      include: 'items',
+      // raw: true,  //items會被攤平到cart..
+      // nest: true
+    })
+      .then(cart => {
+        console.log('postOrder - from cart: ', cart)
+        return Order.create({
+          name: req.body.name,
+          phone: req.body.phone,
+          address: req.body.address,
+          payment_status: req.body.payment_status,
+          shipping_status: req.body.shipping_status,
+          amount: req.body.amount
+        })
+          .then(order => {
+
+            let results = []
+            for (let i = 0; i < cart.items.length; i++) {
+              results.push(OrderItem.create({
+                OrderId: order.id,
+                price: cart.items[i].price,
+                quantity: cart.items[i].CartItem.quantity,
+                ProductId: cart.items[i].id
+              }))
+            }
+            return Promise.all(results)
+              .then(() => {
+                return res.redirect('/orders')
+              })
+          })
+      })
+  },
+  cancelOrder: (req, res) => {
+    return Order.findByPk(req.params.id)
+      .then(order => {
+        console.log(req.body)
+        order.update({
+          ...req.body,
+          payment_status: '-1',   //為何使用字串?
+          shipping_status: '-1',
+        })
+          .then(order => {
+            return res.redirect('back')
+          })
+      })
   }
 }
 
